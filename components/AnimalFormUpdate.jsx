74eee9798dd69cda63e1fc1d038f1beaf997b411
Dianaanaas <nasibullina.di@yandex.ru>
2024-02-08 16:10:54 +0300

    style:animalAdd

--- components/AnimalFormUpdate.jsx
+++ components/AnimalFormUpdate.jsx
@@ -1,20 +1,32 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-function AnimalFormUpdate({title, animal}) {
+function AnimalFormUpdate({ title, animal }) {
   return (
     <Layout title={title}>
-      <form className="updateAnimal" data-id={animal.id}>
-        <p>Изменение животного</p>
-        <input name="picture" placeholder="img" defaultValue={animal.picture} />
-        <br />
-        <input name="name" placeholder="name" defaultValue={animal.name} />
-        <br />
-        <input name="description" placeholder="description" defaultValue={animal.description} />
-        <br />
-        <button className="UpdateAnimalButton">Изменить</button>
-        <div className="errUpdateAnimal" />
-      </form>
+      <div className="container">
+        <form className="updateAnimal form" data-id={animal.id}>
+          <div className="subtitle">Изменение животного</div>
+          <div className="input-container ic1">
+            <input name="picture" className="input" placeholder="img" defaultValue={animal.picture} />
+            <div className="cut" />
+            <label className="placeholder">Фото</label>
+          </div>
+          <div className="input-container ic2">
+            <input name="name" className="input" placeholder="name" defaultValue={animal.name} />
+            <div className="cut" />
+            <label className="placeholder">Имя животного</label>
+          </div>
+          <div className="input-container ic2">
+            <input name="description" className="input" placeholder="description" defaultValue={animal.description} />
+            <div className="cut" />
+            <label className="placeholder">Описание</label>
+          </div>
+          <button className="UpdateAnimalButton submit">Изменить</button>
+          <div className="errUpdateAnimal" />
+        </form>
+      </div>
+
     </Layout>
   );
 }
