2d5d2e2ee8d5814920e10c2032454db58eec29a7
Dianaanaas <nasibullina.di@yandex.ru>
2024-02-09 15:28:30 +0300

    seedAll

---
+++ components/CommentList.jsx
@@ -0,0 +1,14 @@
+const React = require("react");
+
+function CommentList({}) {
+    return(
+<div>
+    <form action="">
+        <input type="name" />
+        <input type="comment" />
+    </form>
+</div>
+    )
+}
+
+module.exports = CommentList
